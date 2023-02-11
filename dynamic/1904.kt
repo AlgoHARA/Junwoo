48cd6f0decde23b7f9d6ffd4746232dce7137265
JunwooKim <rhffldkt0517@naver.com>
2023-02-11 17:09:49 +0900

    [bj1904] 01타일

---
+++ dynamic/1904.kt
@@ -0,0 +1,20 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+/*
+1904 01타일
+*/
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val n = readLine().toInt()
+
+    val list = mutableListOf<Int>()
+    list.add(0)
+    list.add(1)
+    list.add(2)
+    for (i in 3 until n + 1) {
+        list.add((list[i - 1] + list[i - 2]) % 15746)
+    }
+    print(list[n])
+
+}
