35794e586b226bfdadf64ae393790fa6cb2fb2ad
JunwooKim <rhffldkt0517@naver.com>
2023-02-17 23:48:43 +0900

    [bj2606] 바이러스

--- dfs/bj2606.kt
+++ dfs/bj2606.kt
@@ -4,14 +4,15 @@ import java.io.InputStreamReader
 /*
 2606 바이러스
 */
+
 fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
 
-    val n = readLine().toInt()
-    val m = readLine().toInt()
-    val graph = Array<ArrayList<Int>>(n+1) { ArrayList() }
-    val visited = BooleanArray(m+1) { false }
+    val n = readLine().toInt() // 노드
+    val m = readLine().toInt() // 엣지
+    val graph = Array<ArrayList<Int>>(n + 1) { ArrayList() } // 그래프를 이중배열의 형식으로 구현
+    val visited = BooleanArray(n + 1) { false } // 방문한 컴퓨터를 리스트로 표현
 
-    repeat(m) {
+    repeat(m) { // 연결된 엣지 만큼 그래프에 해당 노드에 에 어디어디가 연결되었는지 리스트 형태에 더해줌
         val (node, near) = readLine().split(" ").map { it.toInt() }
         graph[node].add(near)
         graph[near].add(node)
@@ -19,16 +20,12 @@ fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
 
     fun dfs(index: Int) {
         visited[index] = true
-        graph[index].forEach{
+        graph[index].forEach {
             if (!visited[it]) {
                 dfs(it)
             }
         }
     }
-    if(n == 1 || m == 0)
-        print(0)
-    else{
-        dfs(1)
-        print(visited.count { it } - 1)
-    }
+    dfs(1)
+    print(visited.count() { it } - 1) // 1부터 시작하여 방문해서 visited가 true가 된 수 - 시작지점(1)
 }
