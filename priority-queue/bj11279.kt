acc563dee193d493e469c91008627737dcef3534
JunwooKim <rhffldkt0517@naver.com>
2023-02-24 20:17:58 +0900

    [bj11279] 최대 힙

---
+++ priority-queue/bj11279.kt
@@ -0,0 +1,23 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.StringBuilder
+import java.util.Collections
+import java.util.PriorityQueue
+
+/**
+ * 11279 최대 힙
+ */
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val sb = StringBuilder()
+    val heap = PriorityQueue<Int>(Collections.reverseOrder())
+    repeat(readLine().toInt()) {
+        val num = readLine().toInt()
+        if (num == 0) {
+            if (heap.isNotEmpty()) sb.append("${heap.poll()}\n")
+            else sb.append("0\n")
+        } else {
+            heap.add(num)
+        }
+    }
+    print(sb)
+}
