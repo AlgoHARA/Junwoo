5cb1a95bbb1ac00ae1bc233591e25b40efcb3aa6
JunwooKim <rhffldkt0517@naver.com>
2023-04-16 15:42:21 +0900

    [bj15666]N과 M(12)

---
+++ backtracking/bj15666.kt
@@ -0,0 +1,23 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+/*
+* 15666 N과 M(12)
+*/
+
+fun main() = with(BufferedReader(InputStreamReader(System.`in`))) {
+    val (n, m) = readLine().split(" ").map { it.toInt() }
+    val list = readLine().split(" ").map { it.toInt() }.sorted()
+    val result = mutableSetOf<String>()
+    fun backtrack(nums: String, start: Int, depth: Int) {
+        if (depth == m) {
+            result.add(nums.trim())
+            return
+        }
+        for (i in start..n - 1) {
+                backtrack("$nums${list[i]} ", i, depth + 1)
+        }
+    }
+    backtrack("", 0, 0)
+    print(result.joinToString("\n"))
+}
