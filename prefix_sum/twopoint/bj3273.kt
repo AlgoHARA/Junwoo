32703367b1585c42bff9ae5996e7ef53f0ea1232
JunwooKim <rhffldkt0517@naver.com>
2024-02-02 00:52:37 +0900

    bj3273 두수의 합

---
+++ prefix_sum/twopoint/bj3273.kt
@@ -0,0 +1,22 @@
+fun main() {
+    val n = readln().toInt()
+    val list = readln().split(" ").map { it.toInt() }.toMutableList()
+    val x = readln().toInt()
+    list.removeIf { it > x }
+    list.sort()
+
+    var start = 0
+    var end = list.size - 1
+    var sum = 0
+    var count = 0
+
+    while (start < end) {
+        sum = list[start] + list[end]
+        if (sum == x) count++
+
+        if (sum >= x) end--
+        else start++
+    }
+
+    print(count)
+} // 누적합은 아니지만 투포인터를 활용하여 O(n^2)이 아닌 O(n)으로 풀이
